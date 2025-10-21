79ddb32056643e19f374cd83b01f63ed2a25813b
junminhong <junminhong1110@gmail.com>
2025-10-21 22:11:07 +0800

    feat(vite.config.ts): add vite config file

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vite'
+import viteReact from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+
+export default defineConfig({
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    viteReact(),
+    tailwindcss(),
+    tsconfigPaths(),
+  ],
+  base: process.env.NODE_ENV === 'production' ? '/jobs/' : '/',
+})
